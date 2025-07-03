ea800eb003b2859c85c0446a8ef28ad391d54cd5
Nagashbek Miras <mirasnag@unist.ac.kr>
2025-07-03 16:11:52 +0900

    - Added Game Buttons in HomePage with initial styling - Added GamePage to routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+// main library
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+// components
 import HomePage from "./pages/home/HomePage";
+import GamePage from "./pages/game/GamePage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/game",
+    element: <GamePage />,
   },
 ]);
 
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,12 +1,29 @@
+// main library
+import { useNavigate } from "react-router-dom";
+
+// components
 import StarfieldBackground from "@components/StarfieldBackground";
+
+// styles
 import classes from "./HomePage.module.scss";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleNewGameClick = () => {
+    navigate("game");
+  };
+
   return (
     <div className="page">
       <StarfieldBackground />
       <div className={classes.layout}>
-        <div className=""></div>
+        <div className={classes.gameButtons}>
+          <button className={classes.button} onClick={handleNewGameClick}>
+            New Game
+          </button>
+          <button className={classes.button}>Resume Game</button>
+        </div>
       </div>
     </div>
   );
