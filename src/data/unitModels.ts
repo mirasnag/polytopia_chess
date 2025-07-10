c31df5e99f74ed965078ece2e4c034fd149877d0
Nagashbek Miras <mirasnag@unist.ac.kr>
2025-07-10 13:02:13 +0900

    - Uploaded Unit Full Body Models

---
+++ src/data/unitModels.ts
@@ -0,0 +1,27 @@
+import type { UnitType } from "@/types/unit";
+import warriorFullbodyModel from "@/assets/unit_models/fullbody/warrior.webp";
+import archerFullbodyModel from "@/assets/unit_models/fullbody/archer.webp";
+import riderFullbodyModel from "@/assets/unit_models/fullbody/rider.webp";
+import catapultFullbodyModel from "@/assets/unit_models/fullbody/catapult.webp";
+import knightFullbodyModel from "@/assets/unit_models/fullbody/knight.webp";
+import swordsmanFullbodyModel from "@/assets/unit_models/fullbody/swordsman.webp";
+import mindBenderFullbodyModel from "@/assets/unit_models/fullbody/mindBender.webp";
+
+interface UnitModels {
+  fullbody: string;
+}
+
+const UNIT_MODELS: Record<UnitType, UnitModels> = {
+  warrior: { fullbody: warriorFullbodyModel },
+  archer: { fullbody: archerFullbodyModel },
+  rider: { fullbody: riderFullbodyModel },
+  catapult: { fullbody: catapultFullbodyModel },
+  knight: { fullbody: knightFullbodyModel },
+  swordsman: { fullbody: swordsmanFullbodyModel },
+  mindBender: { fullbody: mindBenderFullbodyModel },
+};
+
+export const getUnitModels = () => UNIT_MODELS;
+
+export const getUnitFullbodyModel = (unitType: UnitType) =>
+  UNIT_MODELS[unitType].fullbody;
